a6b5b4e4bb6fc5a803a97bee2e22abe69407ab22
LSKeegan <45316281+LSKeegan@users.noreply.github.com>
2019-06-14 22:32:18 -0400

    Add project files.

---
+++ KeeganSheedyWebApp/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="KeeganSheedyWebApp.MvcApplication" Language="C#" %>
---
+++ KeeganSheedyWebApp/Models/ContactFormModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace KeeganSheedyWebApp.Models
+{
+    public class ContactFormModel
+    {
+        [Required]
+        public string FirstName { get; set; }
+        [Required]
+        public string LastName { get; set; }
+        [Required]
+        public string EmailAddress { get; set; }
+        [Required]
+        public string Subject { get; set; }
+        [Required]
+        public string Message { get; set; }
+
+    }
+}
